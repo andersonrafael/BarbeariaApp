3277e78d16606c38119fb3e6cffebf7a375d080b
anderson rafael <franciscoandersonrafael@gmail.com>
2023-09-09 20:05:47 -0300

    sabado

--- app/src/main/java/com/example/barbeariaapp/MainActivity.kt
+++ app/src/main/java/com/example/barbeariaapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.barbeariaapp
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.view.View
 import com.example.barbeariaapp.databinding.ActivityMainBinding
+import com.example.barbeariaapp.view.Home
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
@@ -21,17 +23,32 @@ class MainActivity : AppCompatActivity() {
 
             val nome = binding.editeNome.text.toString()
             val senha = binding.editeSenha.text.toString()
+
             when{
                 nome.isEmpty() -> {
-                    mensagem(it, mensagem:"Coloque Seu Nome!")
+                    mensagem(it,"Coloque Seu Nome!")
+                }
+                senha.isEmpty() ->{
+                    mensagem(it,"Digite sua senha!")
+                }
+                senha.length <= 5 -> {
+                    mensagem(it,"A senha precisa ter pelo menos 6 caracteres!")
+                }
+                else ->{
+                    navegarPraHome(nome)
                 }
             }
         }
     }
-    private fun mensagem(view: view, mensagem: String){
+    private fun mensagem(view: View, mensagem: String){
         val snackbar = Snackbar.make(view,mensagem,Snackbar.LENGTH_SHORT)
-        snackbar.setBackgroundTint(Color.parseColor(colorString:"#FF0000"))
-        snackbar.setTextColor(Color.parseColor(colorString:"#FFFFFF"))
+        snackbar.setBackgroundTint(Color.parseColor("#FF0000"))
+        snackbar.setTextColor(Color.parseColor("#FFFFFF"))
         snackbar.show()
+    }
+    private fun navegarPraHome(nome: String){
+        val intent = Intent(this,Home::class.java)
+        intent.putExtra("nome",nome)
+        startActivity(intent)
     }
 }
